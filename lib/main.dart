ea8e5caf066cc9aef49da37abe85bd0be43a46ca
8xc0m4 <fhzlfhzl0307@gmail.com>
2025-09-16 22:02:44 +0900

    상록: 에셋 머지하기 전 중간 세이브

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ja_chwi/core/config/router/router.dart';
+import 'package:ja_chwi/presentation/screens/auth/login_screen.dart';
+import 'package:ja_chwi/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router, // go_router
-      title: '자취의정석',
-      debugShowCheckedModeBanner: false,
-    );
+    // return MaterialApp.router(
+    //   routerConfig: router, // go_router
+    //   title: '자취의정석',
+    //   debugShowCheckedModeBanner: false,
+    // );
+    return MaterialApp(home: LoginScreen());
   }
 }
