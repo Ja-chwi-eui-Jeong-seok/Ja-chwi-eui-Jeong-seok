595a95948d177437b68edd446c69c19f64800521
8xc0m4 <fhzlfhzl0307@gmail.com>
2025-09-16 22:02:44 +0900

    상록: 에셋 머지하기 전 중간 세이브

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -6,9 +6,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('HomeScreen')),
-      body: const Center(
-        child: Text('HomeScreen Screen'),
+      appBar: AppBar(title: const Text('자취의 정석')),
+      body: Column(
+        children: [
+          //레벨
+          Container(
+            width: double.infinity,
+            height: 80,
+            padding: const EdgeInsets.all(16),
+            color: Colors.blue,
+          ),
+
+          //미션
+          Container(
+            width: double.infinity,
+            height: 100,
+            padding: const EdgeInsets.all(16),
+            color: Colors.green,
+          ),
+          SizedBox(height: 80),
+
+          //캐릭터
+          Stack(
+            children: [
+              Container(
+                width: 142,
+                height: 162,
+                padding: const EdgeInsets.all(16),
+                color: Colors.orange,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
