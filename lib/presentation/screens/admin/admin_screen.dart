7e67e9c9cd3e4787baab6b3eae15ad598b2f2c07
chori0204 <chori0204@gmail.com>
2025-09-17 16:38:27 +0900

    chore: 이미지 파일 경로수정             readme 수정 add : 프로필 페이지 추가

---
+++ lib/presentation/screens/admin/admin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminScreen extends StatelessWidget {
+  const AdminScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('AdminScreen')),
+      body: const Center(
+        child: Text('AdminScreen Screen'),
+      ),
+    );
+  }
+}
