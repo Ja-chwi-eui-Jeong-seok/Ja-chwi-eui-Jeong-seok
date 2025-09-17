810cfa1b5a3c5f308096dd28db6b7ba3a4d0e67f
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-09-17 16:47:27 +0900

    add:dto

--- lib/core/config/router/router.dart
+++ lib/core/config/router/router.dart
@@ -23,7 +23,7 @@ import 'package:ja_chwi/presentation/screens/profile/profile_screen.dart';
 //라우트만 추가되면 RouteTitles.map에 새 경로를 매핑해주면 된다. 화면마다 수정할 필요 없음.
 
 final GoRouter router = GoRouter(
-  initialLocation: '/login',
+  initialLocation: '/community',
   routes: [
     GoRoute(
       path: '/login',
