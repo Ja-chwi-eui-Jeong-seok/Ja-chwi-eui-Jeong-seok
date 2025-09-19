915409c6cbc9683950cd57b0fa5bcf4d255eeaac
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-09-19 23:10:14 +0900

    feat:댓글영역수정

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,7 @@ import 'package:ja_chwi/presentation/screens/auth/login_screen.dart';
 import 'package:ja_chwi/presentation/screens/home/home_screen.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,11 +14,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp.router(
-    //   routerConfig: router, // go_router
-    //   title: '자취의정석',
-    //   debugShowCheckedModeBanner: false,
-    // );
-    return MaterialApp(home: HomeScreen());
+    return MaterialApp.router(
+      routerConfig: router, // go_router
+      title: '자취의정석',
+      debugShowCheckedModeBanner: false,
+    );
+    // return MaterialApp(home: HomeScreen());
   }
 }
