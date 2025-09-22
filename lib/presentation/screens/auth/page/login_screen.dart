75be51dcde68b0773d1306f3128925a6ff88554f
8xc0m4 <fhzlfhzl0307@gmail.com>
2025-09-22 16:19:51 +0900

    ✨ 쉬기전 저장

--- lib/presentation/screens/auth/page/login_screen.dart
+++ lib/presentation/screens/auth/page/login_screen.dart
@@ -14,7 +14,6 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            // 센터 이미지
             const SizedBox(height: 40),
             Expanded(
               child: Center(
@@ -22,7 +21,6 @@ class LoginScreen extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     WaveText(text: '자취의 정석 \n 시작하기'),
-
                     const SizedBox(height: 40),
                     Image.asset(
                       'assets/images/profile/black.png',
@@ -35,8 +33,14 @@ class LoginScreen extends StatelessWidget {
             ),
             LoginButton(
               onLoginSuccess: () async {
+                // 1️⃣ 개인정보 처리방침 화면으로 이동
                 final accepted = await context.push<bool>('/privacy-policy');
+
                 if (accepted == true) {
+                  // 2️⃣ 프로필 화면으로 이동
+                  await context.push('/profile');
+
+                  // 3️⃣ 프로필 완료 후 홈으로 이동
                   context.go('/home');
                 }
               },
