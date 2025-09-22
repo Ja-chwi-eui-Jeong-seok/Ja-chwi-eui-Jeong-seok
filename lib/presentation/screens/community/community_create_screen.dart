b4a6240cd5449461b0a9ac6f5146a93d096596ab
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-09-22 17:22:26 +0900

    feat:상세 수정

--- lib/presentation/screens/community/community_create_screen.dart
+++ lib/presentation/screens/community/community_create_screen.dart
@@ -12,6 +12,9 @@ class CommunityCreateScreen extends ConsumerStatefulWidget {
 
 class _CommunityCreateScreenState extends ConsumerState<CommunityCreateScreen> {
   CommunityCreateVm get vm => ref.read(communityCreateVmProvider);
+  final selectedCategoryProvider = StateProvider<String?>((ref) => null);
+  final selectedSubCategoryProvider = StateProvider<String?>((ref) => null);
+
   final _titleController = TextEditingController();
   final _contentController = TextEditingController();
 
@@ -21,6 +24,7 @@ class _CommunityCreateScreenState extends ConsumerState<CommunityCreateScreen> {
 
   // 카테고리별 세부 카테고리 매핑
   final Map<String, List<String>> subCategories = {
+    //카테고리 불러오고 , 서브카테고리 맵핑하기
     '요리': ['한식', '양식', '디저트', '자유'],
     '운동': ['크로스핏', '스트릿', '헬스', '스포츠', '자유'],
     '청소': ['방청소', '주방청소', '욕실청소', '자유'],
@@ -30,6 +34,8 @@ class _CommunityCreateScreenState extends ConsumerState<CommunityCreateScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final selectedCategory = ref.watch(selectedCategoryProvider);
+    final selectedSubCategory = ref.watch(selectedSubCategoryProvider);
     return Scaffold(
       appBar: AppBar(title: const Text("글쓰기")),
       body: SingleChildScrollView(
@@ -83,48 +89,56 @@ class _CommunityCreateScreenState extends ConsumerState<CommunityCreateScreen> {
             ),
 
             //Center로 가운데 정렬하고 SizedBox로 크기제한,Wrap의 자동줄바꿈 가능하게만듦
-            Center(
-              child: SizedBox(
-                width: double.infinity,
-                child: Wrap(
-                  runSpacing: 10,
-                  alignment: WrapAlignment.center,
-                  runAlignment: WrapAlignment.center,
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  spacing: 8,
-                  children: ['자유', '운동', '청소', '미션', '요리']
-                      .map(
-                        (c) => ChoiceChip(
-                          label: SizedBox(
-                            width: 38,
-                            height: 38,
-                            child: Center(child: Text(c)),
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadiusGeometry.circular(15),
-                          ),
-                          side: BorderSide(color: Colors.black),
-                          selected: selectedCategory == c,
-                          onSelected: (_) {
-                            setState(() {
-                              selectedCategory = c;
-                              selectedSubCategory = null; // 초기화
-                            });
-                          },
-                          selectedColor: Colors.black,
-                          labelStyle: TextStyle(
-                            color: selectedCategory == c
-                                ? Colors.white
-                                : Colors.black,
-                          ),
-                          backgroundColor: Colors.white,
-                          showCheckmark: false,
-                        ),
-                      )
-                      .toList(),
-                ),
+            SizedBox(
+              height: 55, // 칩 높이 + 여백
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                children:
+                    ['자유', '운동', '청소', '미션', '요리'] //TODO:카테고리코드 불러오기
+                        .map((c) {
+                          final isSelected = selectedCategory == c;
+                          return Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: ChoiceChip(
+                              label: SizedBox(
+                                width: 38,
+                                height: 38,
+                                child: Center(child: Text(c)),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15), // ✅ 수정
+                              ),
+                              side: const BorderSide(color: Colors.black),
+                              selected: selectedCategory == c,
+                              onSelected: (_) {
+                                ref
+                                    .read(selectedCategoryProvider.notifier)
+                                    .state = isSelected
+                                    ? null
+                                    : c; // 같은 칩 누르면 해제 옵션
+                                ref
+                                        .read(
+                                          selectedSubCategoryProvider.notifier,
+                                        )
+                                        .state =
+                                    null;
+                              },
+                              selectedColor: Colors.black,
+                              labelStyle: TextStyle(
+                                color: selectedCategory == c
+                                    ? Colors.white
+                                    : Colors.black,
+                              ),
+                              backgroundColor: Colors.white,
+                              showCheckmark: false,
+                            ),
+                          );
+                        })
+                        .toList(),
               ),
             ),
+
             SizedBox(height: 30),
             Divider(
               height: 2,
@@ -144,106 +158,156 @@ class _CommunityCreateScreenState extends ConsumerState<CommunityCreateScreen> {
                 width: double.infinity,
                 child: Wrap(
                   alignment: WrapAlignment.center,
-                  //runAlignment: WrapAlignment.center,
-                  //crossAxisAlignment: WrapCrossAlignment.center,
                   spacing: 8,
-                  children: subCategories[selectedCategory]!
-                      .map(
-                        (sub) => ChoiceChip(
-                          label: SizedBox(height: 20, child: Text(sub)),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadiusGeometry.circular(15),
-                          ),
-                          side: BorderSide(color: Colors.black),
-                          selected: selectedSubCategory == sub,
-                          onSelected: (_) {
-                            setState(() {
-                              selectedSubCategory = sub;
-                            });
-                          },
-                          selectedColor: Colors.black,
-                          labelStyle: TextStyle(
-                            color: selectedSubCategory == sub
-                                ? Colors.white
-                                : Colors.black,
-                          ),
-                          backgroundColor: Colors.white,
-                          showCheckmark: false,
-                        ),
-                      )
-                      .toList(),
+                  children: (subCategories[selectedCategory] ?? []).map((sub) {
+                    final isSelected = selectedSubCategory == sub;
+                    return ChoiceChip(
+                      label: SizedBox(height: 20, child: Text(sub)),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusGeometry.circular(15),
+                      ),
+                      side: BorderSide(color: Colors.black),
+                      selected: selectedSubCategory == sub,
+                      onSelected: (_) {
+                        ref.read(selectedSubCategoryProvider.notifier).state =
+                            isSelected ? null : sub;
+                      },
+                      selectedColor: Colors.black,
+                      labelStyle: TextStyle(
+                        color: selectedSubCategory == sub
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                      backgroundColor: Colors.white,
+                      showCheckmark: false,
+                    );
+                  }).toList(),
                 ),
               ),
             ],
+            SizedBox(height: 32),
             Divider(
               height: 2,
               thickness: 2,
             ),
-            //selectedSubCategory = sub
             SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.only(left: 24, right: 24, bottom: 50),
+              child: SizedBox(
+                width: double.infinity,
+                child: GestureDetector(
+                  onTap: () async {
+                    //테스트용 프린트
+                    print("제목: ${_titleController.text}");
+                    print("내용: ${_contentController.text}");
+                    print("카테고리: $selectedCategory");
+                    print("세부 카테고리: $selectedSubCategory");
 
-            //Spacer(),
-            // 확인 버튼
-          ],
-        ),
-      ),
-      bottomSheet: Padding(
-        padding: const EdgeInsets.only(left: 24, right: 24, bottom: 50),
-        child: SizedBox(
-          width: double.infinity,
-          child: GestureDetector(
-            onTap: () async {
-              //테스트용 프린트
-              print("제목: ${_titleController.text}");
-              print("내용: ${_contentController.text}");
-              print("카테고리: $selectedCategory");
-              print("세부 카테고리: $selectedSubCategory");
+                    //유효성검증
+                    final err = await vm.submit(
+                      title: _titleController.text,
+                      content: _contentController.text,
+                      category: selectedCategory,
+                      subCategory: selectedSubCategory,
+                    );
 
-              //유효성검증
-              final err = await vm.submit(
-                title: _titleController.text,
-                content: _contentController.text,
-                category: selectedCategory,
-                subCategory: selectedSubCategory,
-              );
-
-              if (!context.mounted) return;
-              if (err == null) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('등록완료'),
-                  ),
-                );
-                //TODO: 로딩 후 작성글한 글로 이동
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(err),
-                  ),
-                );
-              }
-            },
-            child: Container(
-              width: 300,
-              height: 55,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Center(
-                child: Text(
-                  "확인",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
+                    if (!context.mounted) return;
+                    if (err == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('등록완료'),
+                        ),
+                      );
+                      //TODO: 로딩 후 작성글한 글로 이동
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(err),
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    width: 300,
+                    height: 55,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "확인",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
+          ],
         ),
       ),
+      // bottomSheet: Padding(
+      //   padding: const EdgeInsets.only(left: 24, right: 24, bottom: 50),
+      //   child: SizedBox(
+      //     width: double.infinity,
+      //     child: GestureDetector(
+      //       onTap: () async {
+      //         //테스트용 프린트
+      //         print("제목: ${_titleController.text}");
+      //         print("내용: ${_contentController.text}");
+      //         print("카테고리: $selectedCategory");
+      //         print("세부 카테고리: $selectedSubCategory");
+
+      //         //유효성검증
+      //         final err = await vm.submit(
+      //           title: _titleController.text,
+      //           content: _contentController.text,
+      //           category: selectedCategory,
+      //           subCategory: selectedSubCategory,
+      //         );
+
+      //         if (!context.mounted) return;
+      //         if (err == null) {
+      //           ScaffoldMessenger.of(context).showSnackBar(
+      //             SnackBar(
+      //               content: Text('등록완료'),
+      //             ),
+      //           );
+      //           //TODO: 로딩 후 작성글한 글로 이동
+      //         } else {
+      //           ScaffoldMessenger.of(context).showSnackBar(
+      //             SnackBar(
+      //               content: Text(err),
+      //             ),
+      //           );
+      //         }
+      //       },
+      //       child: Container(
+      //         width: 300,
+      //         height: 55,
+      //         decoration: BoxDecoration(
+      //           color: Colors.white,
+      //           border: Border.all(),
+      //           borderRadius: BorderRadius.circular(25),
+      //         ),
+      //         child: Center(
+      //           child: Text(
+      //             "확인",
+      //             style: TextStyle(
+      //               fontSize: 20,
+      //               fontWeight: FontWeight.w500,
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
